24b468a821a4c899ecdb76a6e63791edbe5f86dd
Devin Abbott <devinabbott@gmail.com>
2021-02-07 10:11:13 -0800

    Add color picker popover

--- packages/app/src/components/ColorInputField.tsx
+++ packages/app/src/components/ColorInputField.tsx
@@ -1,7 +1,9 @@
 import type FileFormat from '@sketch-hq/sketch-file-format-ts';
 import styled from 'styled-components';
+import * as Popover from '@radix-ui/react-popover';
+import DualAxisColorPicker from './color/DualAxisColorPicker';
 
-const Row = styled.div<{ color: string }>(({ theme, color }) => ({
+const Trigger = styled(Popover.Trigger)(({ color }) => ({
   width: '60px',
   height: '27px',
   borderRadius: '4px',
@@ -9,18 +11,37 @@ const Row = styled.div<{ color: string }>(({ theme, color }) => ({
   backgroundColor: color,
 }));
 
+const StyledContent = styled(Popover.Content)({
+  width: '240px',
+  borderRadius: 4,
+  padding: '10px',
+  fontSize: 14,
+  backgroundColor: 'white',
+  color: 'black',
+  boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
+});
+
+const StyledArrow = styled(Popover.Arrow)({
+  fill: 'white',
+});
+
 interface Props {
   id?: string;
   color: FileFormat.Color;
 }
 
 export default function ColorInputField({ id, color }: Props) {
+  const colorString = `rgba(${color.red * 255}, ${color.green * 255}, ${
+    color.blue * 255
+  }, ${color.alpha})`;
+
   return (
-    <Row
-      id={id}
-      color={`rgba(${color.red * 255}, ${color.green * 255}, ${
-        color.blue * 255
-      }, ${color.alpha})`}
-    />
+    <Popover.Root>
+      <Trigger color={colorString} id={id} />
+      <StyledContent>
+        <DualAxisColorPicker color={{ h: 0, s: 1, l: 0.5, a: 1 }} />
+        <StyledArrow />
+      </StyledContent>
+    </Popover.Root>
   );
 }
