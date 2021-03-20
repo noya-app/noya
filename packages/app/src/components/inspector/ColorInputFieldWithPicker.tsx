83a677bb87d06a03338f24df3610ac5902c9f025
Maria Isabel Lopez <mrlpz1515@gmail.com>
2021-03-20 14:09:35 -0500

    design and action completed (#8)

--- packages/app/src/components/inspector/ColorInputFieldWithPicker.tsx
+++ packages/app/src/components/inspector/ColorInputFieldWithPicker.tsx
@@ -2,16 +2,21 @@ import { GridIcon, RowsIcon } from '@radix-ui/react-icons';
 import * as Popover from '@radix-ui/react-popover';
 import { Slot } from '@radix-ui/react-slot';
 import type Sketch from '@sketch-hq/sketch-file-format-ts';
+import { useApplicationState } from '../../contexts/ApplicationStateContext';
 import {
   ColorInputField,
   Divider,
   RadioGroup,
   Select,
   Spacer,
+  sketchColorToRgba,
+  ListView,
 } from 'noya-designsystem';
+import { getSharedSwatches } from 'noya-state/src/selectors';
 import { memo, useCallback, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import ColorInspector from './ColorInspector';
+import { hsvaToRgbaString, rgbaToHsva } from 'noya-colorpicker';
 
 const Content = styled(Popover.Content)(({ theme }) => ({
   width: '240px',
@@ -44,6 +49,25 @@ const RadioGroupContainer = styled.div({
   alignItems: 'stretch',
 });
 
+const Square = styled.div<{ color: string; selected?: boolean }>(
+  ({ theme, color, selected = false }) => ({
+    height: '25px',
+    width: '25px',
+    backgroundColor: color,
+    border: `2px solid ${
+      selected ? 'rgb(132,63,255)' : theme.colors.popover.background
+    } `,
+    borderRadius: '4px',
+    cursor: 'pointer',
+  }),
+);
+
+const GridSmall = styled.div({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fill, 25px)',
+  gap: '5px',
+});
+
 type SwatchLayout = 'list' | 'grid';
 
 interface Props {
@@ -51,6 +75,72 @@ interface Props {
   value: Sketch.Color;
   onChange: (color: Sketch.Color) => void;
 }
+
+interface GridProps {
+  id?: string;
+  swatches: Sketch.Swatch[];
+  layout: SwatchLayout;
+  onClick: (color: Sketch.Color) => void;
+}
+
+const SwatchesSelector = memo(function SwatchesSelector({
+  swatches,
+  layout,
+  onClick,
+}: GridProps) {
+  const [selected, setSelected] = useState<string>('');
+
+  return (
+    <>
+      {layout === 'grid' && (
+        <GridSmall>
+          {swatches.map((item) => {
+            const colorString = hsvaToRgbaString(
+              rgbaToHsva(sketchColorToRgba(item.value)),
+            );
+
+            return (
+              <Square
+                key={item.do_objectID}
+                color={colorString}
+                selected={selected === item.do_objectID}
+                onClick={() => {
+                  onClick(item.value);
+                  setSelected(item.do_objectID);
+                }}
+              />
+            );
+          })}
+        </GridSmall>
+      )}
+      {layout === 'list' && (
+        <ListView.Root>
+          {swatches.map((item) => {
+            const colorString = hsvaToRgbaString(
+              rgbaToHsva(sketchColorToRgba(item.value)),
+            );
+
+            return (
+              <ListView.Row
+                id={item.do_objectID}
+                key={item.do_objectID}
+                selected={selected === item.do_objectID}
+                onClick={() => {
+                  onClick(item.value);
+                  setSelected(item.do_objectID);
+                }}
+              >
+                <Square color={colorString} />
+                <Spacer.Horizontal size={8} />
+                {item.name}
+              </ListView.Row>
+            );
+          })}
+        </ListView.Root>
+      )}
+    </>
+  );
+});
 
 export default memo(function ColorInputFieldWithPicker({
   id,
@@ -60,8 +150,11 @@ export default memo(function ColorInputFieldWithPicker({
   // TODO: The value prop here can be an array, and other
   // inspector rows may also take arrays
   const values = useMemo(() => [value], [value]);
+  const [state] = useApplicationState();
 
   const [swatchLayout, setSwatchLayout] = useState<SwatchLayout>('grid');
+
+  const sharedSwatches = getSharedSwatches(state);
 
   return (
     <Popover.Root>
@@ -105,6 +198,14 @@ export default memo(function ColorInputFieldWithPicker({
               </RadioGroup.Root>
             </RadioGroupContainer>
           </Row>
+        </PaddedSection>
+        <Divider />
+        <PaddedSection>
+          <SwatchesSelector
+            layout={swatchLayout}
+            swatches={sharedSwatches}
+            onClick={useCallback((color) => onChange(color), [onChange])}
+          />
         </PaddedSection>
         <StyledArrow />
       </Content>
