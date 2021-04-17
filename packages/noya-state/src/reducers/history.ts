fcc832d711f0b91a8c5f76f318d3acb8d26b6da0
souporserious <ftntravis@gmail.com>
2021-04-17 12:30:16 -0700

    add history entry to compare previous timestamp

--- packages/noya-state/src/reducers/history.ts
+++ packages/noya-state/src/reducers/history.ts
@@ -2,11 +2,16 @@ import produce from 'immer';
 import { ApplicationState, Action, reducer } from './application';
 import { createInitialInteractionState } from './interaction';
 
+type HistoryEntry = {
+  actionType: Action[0];
+  timestamp: number;
+  state: ApplicationState;
+};
+
 export type HistoryState = {
-  past: Array<ApplicationState>;
+  past: HistoryEntry[];
   present: ApplicationState;
-  future: Array<ApplicationState>;
-  timestamp?: number;
+  future: HistoryEntry[];
 };
 
 export type HistoryAction = [type: 'undo'] | [type: 'redo'] | Action;
@@ -14,14 +19,18 @@ export type HistoryAction = [type: 'undo'] | [type: 'redo'] | Action;
 const FILE_CHANGED_TIMEOUT = 300;
 
 export function historyReducer(state: HistoryState, action: HistoryAction) {
-  switch (action[0]) {
+  const currentPresent = state.present;
+  const [actionType] = action;
+  switch (actionType) {
     case 'undo':
       return produce(state, (state) => {
         if (state.past.length > 0) {
-          const newPresent = state.past.pop();
+          const newPresent = state.past.pop() as HistoryEntry;
           if (newPresent) {
-            state.future.unshift(state.present);
-            state.present = newPresent;
+            state.future.unshift(
+              createHistoryEntry(newPresent.actionType, currentPresent),
+            );
+            state.present = newPresent.state;
           }
         }
       });
@@ -30,32 +39,61 @@ export function historyReducer(state: HistoryState, action: HistoryAction) {
         if (state.future.length > 0) {
           const newPresent = state.future.shift();
           if (newPresent) {
-            state.past.push(state.present);
-            state.present = newPresent;
+            state.past.push(
+              createHistoryEntry(newPresent.actionType, currentPresent),
+            );
+            state.present = newPresent.state;
           }
         }
       });
     default:
-      const currentPresent = state.present;
+      const newPresent = reducer(currentPresent, action as Action);
       return produce(state, (state) => {
-        const newPresent = reducer(currentPresent, action);
-        const newTimestamp = Date.now();
         const sketchFileChanged =
           JSON.stringify(currentPresent.sketch) !==
           JSON.stringify(newPresent.sketch);
-        if (
-          sketchFileChanged &&
-          state.timestamp &&
-          newTimestamp - state.timestamp > FILE_CHANGED_TIMEOUT
-        ) {
-          state.past.push({
-            ...currentPresent,
-            interactionState: createInitialInteractionState(),
-          });
+
+        const historyEntry = createHistoryEntry(actionType, {
+          ...currentPresent,
+          interactionState: createInitialInteractionState(),
+        });
+
+        const pushHistoryEntry = () => {
+          state.past.push(historyEntry);
+        };
+
+        if (sketchFileChanged) {
+          if (state.past.length > 0) {
+            const newTimestamp = Date.now();
+            const previousEntry = state.past[state.past.length - 1];
+            if (
+              newTimestamp - previousEntry.timestamp < FILE_CHANGED_TIMEOUT &&
+              actionType === previousEntry.actionType
+            ) {
+              state.past[state.past.length - 1] = {
+                ...historyEntry,
+                state: previousEntry.state,
+              };
+            } else {
+              pushHistoryEntry();
+            }
+          } else {
+            pushHistoryEntry();
+          }
           state.future = [];
         }
-        state.timestamp = newTimestamp;
         state.present = newPresent;
       });
   }
+}
+
+function createHistoryEntry(
+  actionType: Action[0],
+  state: ApplicationState,
+): HistoryEntry {
+  return {
+    actionType,
+    state,
+    timestamp: Date.now(),
+  };
 }
