e20684c60ba96f8459c58ac94c976844af7a4ee1
Maria Isabel Lopez <mrlpz1515@gmail.com>
2021-05-05 20:07:52 -0500

    text group actions and sidebar

--- packages/noya-state/src/reducers/application.ts
+++ packages/noya-state/src/reducers/application.ts
@@ -1319,9 +1319,20 @@ export function reducer(
     case 'setSelectedTextStyleGroup': {
       const [, id] = action;
       return produce(state, (draft) => {
-        if (action[0] === 'setSelectedSwatchGroup')
-          draft.selectedSwatchGroup = id;
-        else draft.selectedThemeStyleGroup = id;
+        switch (action[0]) {
+          case 'setSelectedSwatchGroup': {
+            draft.selectedSwatchGroup = id;
+            return;
+          }
+          case 'setSelectedThemeStyleGroup': {
+            draft.selectedThemeStyleGroup = id;
+            return;
+          }
+          case 'setSelectedTextStyleGroup': {
+            draft.selectedTextStyleGroup = id;
+            return;
+          }
+        }
       });
     }
     case 'groupSwatches':
@@ -1348,11 +1359,20 @@ export function reducer(
           object.name = newName;
         });
 
-        if (action[0] === 'groupSwatches') draft.selectedSwatchGroup = '';
-        else if (action[0] === 'groupThemeStyles')
-          draft.selectedThemeStyleGroup = '';
-        else if (action[0] === 'groupTextStyles')
-          draft.selectedTextStyleGroup = '';
+        switch (action[0]) {
+          case 'groupSwatches': {
+            draft.selectedSwatchGroup = '';
+            return;
+          }
+          case 'groupThemeStyles': {
+            draft.selectedThemeStyleGroup = '';
+            return;
+          }
+          case 'groupTextStyles': {
+            draft.selectedTextStyleGroup = '';
+            return;
+          }
+        }
       });
     }
     case 'setTextCase':
