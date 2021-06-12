a29be19c592e7ee9c06327121986d50947abfc08
Maria Isabel Lopez <mrlpz1515@gmail.com>
2021-06-12 02:39:51 -0500

    change gradint without drag

--- packages/app/src/components/inspector/ColorInspector.tsx
+++ packages/app/src/components/inspector/ColorInspector.tsx
@@ -35,11 +35,6 @@ interface Props {
   onChangeColor: (color: Sketch.Color) => void;
   onChangeOpacity?: (amount: number) => void;
   onNudgeOpacity?: (amount: number) => void;
-  onChangeGradientColor?: (
-    color: Sketch.Color,
-    index: number,
-    position: number,
-  ) => void;
 }
 
 export default memo(function ColorInspector({
