58c467e6510147d52be9c29cf56868e1b3e2f6be
Devin Abbott <devinabbott@gmail.com>
2021-07-01 10:06:06 -0700

    Use separate components for fill preview

--- packages/noya-designsystem/src/components/FillInputField.tsx
+++ packages/noya-designsystem/src/components/FillInputField.tsx
@@ -1,93 +1,40 @@
 import Sketch from '@sketch-hq/sketch-file-format-ts';
-import { ForwardedRef, forwardRef, useMemo } from 'react';
-import styled, { useTheme } from 'styled-components';
-import { sketchColorToRgbaString } from '../utils/sketchColor';
-import { getGradientBackground } from '../utils/getGradientBackground';
-import { getPatternBackground, SketchPattern } from '../utils/sketchPattern';
-import { useApplicationState } from '../../../app/src/contexts/ApplicationStateContext';
-import { useObjectURL } from '../../../app/src/hooks/useObjectURL';
+import { ForwardedRef, forwardRef, memo } from 'react';
+import styled from 'styled-components';
+import { SketchPattern } from '../utils/sketchPattern';
+import { FillPreviewBackground } from './FillPreviewBackground';
 
-const Container = styled.button<{
-  background: string;
-}>(({ theme, background }) => ({
+const Container = styled.button(({ theme }) => ({
   outline: 'none',
+  padding: 0,
   width: '40px',
   height: '27px',
   borderRadius: '4px',
+  overflow: 'hidden',
   border: 'none',
   boxShadow: `0 0 0 1px ${theme.colors.divider} inset`,
-  background,
+  background: 'transparent',
   '&:focus': {
     boxShadow: `0 0 0 1px ${theme.colors.sidebar.background}, 0 0 0 3px ${theme.colors.primary}`,
   },
+  display: 'flex',
+  alignItems: 'stretch',
 }));
-
-const dotsHorizontalSvg = (fillColor: string) => `
-  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 15 15' fill='${fillColor}'>
-    <path d='M3.625 7.5C3.625 8.12132 3.12132 8.625 2.5 8.625C1.87868 8.625 1.375 8.12132 1.375 7.5C1.375 6.87868 1.87868 6.375 2.5 6.375C3.12132 6.375 3.625 6.87868 3.625 7.5ZM8.625 7.5C8.625 8.12132 8.12132 8.625 7.5 8.625C6.87868 8.625 6.375 8.12132 6.375 7.5C6.375 6.87868 6.87868 6.375 7.5 6.375C8.12132 6.375 8.625 6.87868 8.625 7.5ZM12.5 8.625C13.1213 8.625 13.625 8.12132 13.625 7.5C13.625 6.87868 13.1213 6.375 12.5 6.375C11.8787 6.375 11.375 6.87868 11.375 7.5C11.375 8.12132 11.8787 8.625 12.5 8.625Z'></path>
-  </svg>
-`;
 
 interface Props {
   id?: string;
   value?: Sketch.Color | Sketch.Gradient | SketchPattern;
 }
 
-/*interface PatternProps {
-  id?: string;
-  ref: ForwardedRef<HTMLButtonElement>;
-  image?: Sketch.FileRef | Sketch.DataRef;
-}
-
-const PatternContainter = memo(function PatterContainter({
-  id,
-  ref,
-  image,
-  ...rest
-}: PatternProps) {
-  const [state] = useApplicationState();
-  const backgroundPattern = useObjectURL(
-    getPatternBackground(state.sketch.images, image),
-  );
-
-  const background = `center / cover url(${backgroundPattern})`;
-
-  return <Container ref={ref} background={background} id={id} {...rest} />;
-});*/
-
-export default forwardRef(function FillInputField(
-  { id, value, ...rest }: Props,
-  ref: ForwardedRef<HTMLButtonElement>,
-) {
-  const [state] = useApplicationState();
-  const { inputBackground, placeholderDots } = useTheme().colors;
-
-  const backgroundPattern = useObjectURL(
-    getPatternBackground(
-      state.sketch.images,
-      value && value._class === 'pattern' ? value.image : undefined,
-    ),
-  );
-
-  const background = useMemo(() => {
-    if (!value)
-      return [
-        `center url("data:image/svg+xml;utf8,${dotsHorizontalSvg(
-          placeholderDots,
-        )}") no-repeat`,
-        inputBackground,
-      ].join(',');
-
-    switch (value._class) {
-      case 'color':
-        return sketchColorToRgbaString(value);
-      case 'gradient':
-        return getGradientBackground(value.stops, value.gradientType, 180);
-      case 'pattern': {
-        return `center / cover url(${backgroundPattern})`;
-      }
-    }
-  }, [value, placeholderDots, inputBackground, backgroundPattern]);
-
-  return <Container ref={ref} background={background} id={id} {...rest} />;
-});
+export default memo(
+  forwardRef(function FillInputField(
+    { id, value, ...rest }: Props,
+    ref: ForwardedRef<HTMLButtonElement>,
+  ) {
+    return (
+      <Container ref={ref} id={id} {...rest}>
+        <FillPreviewBackground value={value} />
+      </Container>
+    );
+  }),
+);
---
+++ packages/noya-designsystem/src/components/FillPreviewBackground.tsx
@@ -0,0 +1,92 @@
+import Sketch from '@sketch-hq/sketch-file-format-ts';
+import { memo, useMemo } from 'react';
+import styled, { useTheme } from 'styled-components';
+import { useApplicationState } from '../../../app/src/contexts/ApplicationStateContext';
+import { useObjectURL } from '../../../app/src/hooks/useObjectURL';
+import { getGradientBackground } from '../utils/getGradientBackground';
+import { sketchColorToRgbaString } from '../utils/sketchColor';
+import { SketchPattern } from '../utils/sketchPattern';
+
+const dotsHorizontalSvg = (fillColor: string) => `
+  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 15 15' fill='${fillColor}'>
+    <path d='M3.625 7.5C3.625 8.12132 3.12132 8.625 2.5 8.625C1.87868 8.625 1.375 8.12132 1.375 7.5C1.375 6.87868 1.87868 6.375 2.5 6.375C3.12132 6.375 3.625 6.87868 3.625 7.5ZM8.625 7.5C8.625 8.12132 8.12132 8.625 7.5 8.625C6.87868 8.625 6.375 8.12132 6.375 7.5C6.375 6.87868 6.87868 6.375 7.5 6.375C8.12132 6.375 8.625 6.87868 8.625 7.5ZM12.5 8.625C13.1213 8.625 13.625 8.12132 13.625 7.5C13.625 6.87868 13.1213 6.375 12.5 6.375C11.8787 6.375 11.375 6.87868 11.375 7.5C11.375 8.12132 11.8787 8.625 12.5 8.625Z'></path>
+  </svg>
+`;
+
+const Background = styled.span<{ background: string }>(({ background }) => ({
+  background,
+  flex: 1,
+}));
+
+const HorizontalDotsBackground = memo(function HorizontalDotsBackground() {
+  const { inputBackground, placeholderDots } = useTheme().colors;
+
+  const background = useMemo(
+    () =>
+      [
+        `center url("data:image/svg+xml;utf8,${dotsHorizontalSvg(
+          placeholderDots,
+        )}") no-repeat`,
+        inputBackground,
+      ].join(','),
+    [inputBackground, placeholderDots],
+  );
+
+  return <Background background={background} />;
+});
+
+const PatternPreviewBackground = memo(function PatternPreviewBackground({
+  imageRef,
+}: {
+  imageRef: Sketch.FileRef | Sketch.DataRef;
+}) {
+  const [state] = useApplicationState();
+
+  const url = useObjectURL(state.sketch.images[imageRef._ref]);
+
+  return <Background background={`center / cover url(${url})`} />;
+});
+
+const ColorPreviewBackground = memo(function ColorPreviewBackground({
+  color,
+}: {
+  color: Sketch.Color;
+}) {
+  const background = useMemo(() => sketchColorToRgbaString(color), [color]);
+
+  return <Background background={background} />;
+});
+
+const GradientPreviewBackground = memo(function GradientPreviewBackground({
+  gradient,
+}: {
+  gradient: Sketch.Gradient;
+}) {
+  const background = useMemo(
+    () => getGradientBackground(gradient.stops, gradient.gradientType, 180),
+    [gradient.gradientType, gradient.stops],
+  );
+
+  return <Background background={background} />;
+});
+
+interface Props {
+  value?: Sketch.Color | Sketch.Gradient | SketchPattern;
+}
+
+export const FillPreviewBackground = memo(function FillPreviewBackground({
+  value,
+}: Props) {
+  if (!value) return <HorizontalDotsBackground />;
+
+  switch (value._class) {
+    case 'color':
+      return <ColorPreviewBackground color={value} />;
+    case 'gradient':
+      return <GradientPreviewBackground gradient={value} />;
+    case 'pattern':
+      if (!value.image) return null;
+
+      return <PatternPreviewBackground imageRef={value.image} />;
+  }
+});
