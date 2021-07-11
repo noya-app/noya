3eb110ee0ce46d20c7965b788d89fe86ab7c6523
souporserious <tarnold@frame.io>
2021-07-11 13:06:25 -0700

    trying to configure design system

--- packages/noya-designsystem/src/index.tsx
+++ packages/noya-designsystem/src/index.tsx
@@ -40,6 +40,7 @@ export { default as Tooltip } from './components/Tooltip';
 export { default as ContextMenu } from './components/ContextMenu';
 export { default as DropdownMenu } from './components/DropdownMenu';
 export { default as ScrollArea } from './components/ScrollArea';
+export { default as Stack } from './components/Stack';
 export { SEPARATOR_ITEM } from './components/internal/Menu';
 export type { MenuItem, RegularMenuItem } from './components/internal/Menu';
 
--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -1,9 +1,30 @@
+const path = require('path');
+const designSystemPath = path.resolve(__dirname, '../noya-designsystem/src');
+
 module.exports = {
   webpack(config) {
+    // const tsxLoaderIndex = config.module.rules.findIndex(
+    //   (rule) => console.log(rule),
+    //   // rule.test.includes('tsx'),
+    // );
+    // config.module.rules[tsxLoaderIndex].include.push(designSystemPath);
+
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: ['babel-loader'],
+      include: [designSystemPath],
+      exclude: [path.resolve(__dirname, './src')],
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'noya-designsystem': designSystemPath,
+    };
 
     return config;
   },
