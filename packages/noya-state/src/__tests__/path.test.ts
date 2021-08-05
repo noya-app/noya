c082a092fd7bb3d66c19dbe1f49535568de9125a
Devin Abbott <devinabbott@gmail.com>
2021-08-05 14:03:04 -0700

    Cleanup

--- packages/noya-state/src/__tests__/path.test.ts
+++ packages/noya-state/src/__tests__/path.test.ts
@@ -2,7 +2,12 @@ import type { CanvasKit as CanvasKitType, Path } from 'canvaskit';
 import { loadCanvasKit } from 'noya-renderer';
 import { SketchModel } from 'noya-sketch-model';
 import { round } from 'noya-utils';
-import { parsePathCmds, path, PathCommandVerb } from '../primitives/path';
+import {
+  parsePathCmds,
+  path,
+  PathCommand,
+  PathCommandVerb,
+} from '../primitives/path';
 
 let CanvasKit: CanvasKitType;
 
@@ -10,17 +15,8 @@ beforeAll(async () => {
   CanvasKit = await loadCanvasKit();
 });
 
-function describePathCommand([verb, ...args]: [PathCommandVerb, ...number[]]) {
-  const verbName = {
-    [PathCommandVerb.move]: 'move',
-    [PathCommandVerb.line]: 'line',
-    [PathCommandVerb.quad]: 'quad',
-    [PathCommandVerb.conic]: 'conic',
-    [PathCommandVerb.cubic]: 'cubic',
-    [PathCommandVerb.close]: 'close',
-  } as const;
-
-  return `${verbName[verb]}(${args.map((n) => round(n, 2)).join(', ')})`;
+function describePathCommand([verb, ...args]: PathCommand) {
+  return `${PathCommandVerb[verb]}(${args.map((n) => round(n, 2)).join(', ')})`;
 }
 
 function describePath(path: Path) {
