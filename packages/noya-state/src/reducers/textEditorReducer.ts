76de91ea9a00688835807a82ef8bef6cec4e5a66
Devin Abbott <devinabbott@gmail.com>
2021-08-09 09:34:44 -0700

    Refactor getting next cursor index

--- packages/noya-state/src/reducers/textEditorReducer.ts
+++ packages/noya-state/src/reducers/textEditorReducer.ts
@@ -1,4 +1,4 @@
-import { CanvasKit, ShapedLine } from 'canvaskit';
+import { CanvasKit, Paragraph, ShapedLine } from 'canvaskit';
 import produce from 'immer';
 import { Point } from 'noya-geometry';
 import { Layers, MAX_TEXT_LAYER_STRING_LENGTH, Selectors } from 'noya-state';
@@ -65,10 +65,7 @@ export function textEditorReducer(
 
       if (!state.selectedText) return state;
 
-      const {
-        layerId,
-        range: { head, anchor },
-      } = state.selectedText;
+      const { layerId, range } = state.selectedText;
       const pageIndex = Selectors.getCurrentPageIndex(state);
       const indexPath = Layers.findIndexPath(
         Selectors.getCurrentPage(state),
@@ -87,139 +84,21 @@ export function textEditorReducer(
 
         if (!Layers.isTextLayer(draftLayer)) return;
 
-        const directionMultiplier = direction === 'forward' ? 1 : -1;
-        const string = draftLayer.attributedString.string;
-        const length = string.length;
-
         const paragraph = Selectors.getLayerParagraph(
           CanvasKit,
           context.fontManager,
           draftLayer,
         );
 
-        switch (unit) {
-          case 'character': {
-            // If we have a selected range, move to one side of it
-            if (head !== anchor) {
-              const [min, max] = normalizeRange([head, anchor]);
-              const nextIndex = direction === 'forward' ? max : min;
-              draft.selectedText.range = { anchor: nextIndex, head: nextIndex };
-              break;
-            }
+        const nextIndex = getNextCursorIndex(
+          paragraph,
+          draftLayer.attributedString.string,
+          range,
+          direction,
+          unit,
+        );
 
-            const nextIndex = clamp(head + directionMultiplier, 0, length);
-            draft.selectedText.range = { anchor: nextIndex, head: nextIndex };
-            break;
-          }
-          case 'word':
-            const currentWordBoundary = paragraph.getWordBoundary(head);
-
-            let nextIndex =
-              direction === 'forward'
-                ? currentWordBoundary.end
-                : currentWordBoundary.start;
-
-            // The `start` may be the same as our current `head`, in which
-            // case the cursor wouldn't move.
-            if (direction === 'backward' && nextIndex === head && head > 0) {
-              nextIndex = paragraph.getWordBoundary(head - 1).start;
-            }
-
-            switch (direction) {
-              case 'forward':
-                while (
-                  nextIndex < length &&
-                  !wordRe.test(string[nextIndex - 1])
-                ) {
-                  nextIndex = paragraph.getWordBoundary(nextIndex).end;
-                }
-                break;
-              case 'backward':
-                while (nextIndex > 0 && !wordRe.test(string[nextIndex])) {
-                  nextIndex = paragraph.getWordBoundary(nextIndex - 1).start;
-                }
-                break;
-            }
-
-            draft.selectedText.range = { anchor: nextIndex, head: nextIndex };
-            break;
-          case 'line': {
-            const metrics = paragraph.getLineMetrics();
-            const current = metrics.find(
-              (lm) => lm.startIndex <= head && head <= lm.endIndex,
-            );
-
-            if (!current) break;
-
-            const nextIndex =
-              direction === 'forward' ? current.endIndex : current.startIndex;
-            draft.selectedText.range = { anchor: nextIndex, head: nextIndex };
-            break;
-          }
-          case 'all': {
-            const nextIndex = direction === 'forward' ? length : 0;
-            draft.selectedText.range = { anchor: nextIndex, head: nextIndex };
-            break;
-          }
-          case 'vertical': {
-            const shapedLines = paragraph.getShapedLines();
-
-            const shapedLine = shapedLines.find(
-              (s) => s.textRange.first <= head && head <= s.textRange.last,
-            );
-            // console.log(shapedLine);
-
-            if (shapedLine) {
-              const coordinates = getGlyphCoordinatesForShapedLine(shapedLine);
-              const coordinate = coordinates[head - shapedLine.textRange.first];
-
-              const nextIndex = paragraph.getGlyphPositionAtCoordinate(
-                coordinate.x,
-                // TODO: Instead of 1, can we use the line metrics to find the actual y pos?
-                direction === 'forward'
-                  ? shapedLine.bottom + 1
-                  : shapedLine.top - 1,
-              ).pos;
-
-              draft.selectedText.range = { anchor: nextIndex, head: nextIndex };
-
-              // console.log(newPosition);
-            } else {
-              switch (direction) {
-                case 'forward': {
-                  const shapedLine = shapedLines.find(
-                    (s) => head <= s.textRange.last,
-                  );
-
-                  const nextIndex = shapedLine
-                    ? shapedLine.textRange.first
-                    : length;
-
-                  draft.selectedText.range = {
-                    anchor: nextIndex,
-                    head: nextIndex,
-                  };
-
-                  break;
-                }
-                case 'backward': {
-                  const shapedLine = [...shapedLines]
-                    .reverse()
-                    .find((s) => s.textRange.first <= head);
-
-                  const nextIndex = shapedLine ? shapedLine.textRange.first : 0;
-
-                  draft.selectedText.range = {
-                    anchor: nextIndex,
-                    head: nextIndex,
-                  };
-
-                  break;
-                }
-              }
-            }
-          }
-        }
+        draft.selectedText.range = { anchor: nextIndex, head: nextIndex };
       });
     }
     case 'insertText': {
@@ -262,6 +141,103 @@ export function textEditorReducer(
   }
 }
 
+function getNextCursorIndex(
+  paragraph: Paragraph,
+  string: string,
+  selectionRange: TextSelectionRange,
+  direction: TextEditorCursorDirection,
+  unit: TextEditorCursorUnit,
+): number {
+  const { head, anchor } = selectionRange;
+  const directionMultiplier = direction === 'forward' ? 1 : -1;
+  const length = string.length;
+
+  switch (unit) {
+    case 'character': {
+      // If we have a selected range, move to one side of it
+      if (head !== anchor) {
+        const [min, max] = normalizeRange([head, anchor]);
+        return direction === 'forward' ? max : min;
+      }
+
+      return clamp(head + directionMultiplier, 0, length);
+    }
+    case 'word':
+      const currentWordBoundary = paragraph.getWordBoundary(head);
+
+      let nextIndex =
+        direction === 'forward'
+          ? currentWordBoundary.end
+          : currentWordBoundary.start;
+
+      switch (direction) {
+        case 'forward':
+          while (nextIndex < length && !wordRe.test(string[nextIndex - 1])) {
+            nextIndex = paragraph.getWordBoundary(nextIndex).end;
+          }
+          break;
+        case 'backward':
+          // The `start` may be the same as our current `head`, in which
+          // case the cursor wouldn't move.
+          if (nextIndex === head && head > 0) {
+            nextIndex = paragraph.getWordBoundary(head - 1).start;
+          }
+
+          while (nextIndex > 0 && !wordRe.test(string[nextIndex])) {
+            nextIndex = paragraph.getWordBoundary(nextIndex - 1).start;
+          }
+          break;
+      }
+
+      return nextIndex;
+    case 'line': {
+      const metrics = paragraph.getLineMetrics();
+      const current = metrics.find(
+        (lm) => lm.startIndex <= head && head <= lm.endIndex,
+      );
+
+      if (!current) return head;
+
+      return direction === 'forward' ? current.endIndex : current.startIndex;
+    }
+    case 'all': {
+      return direction === 'forward' ? length : 0;
+    }
+    case 'vertical': {
+      const lineMetadata = getLineMetadata(paragraph);
+      const metadataIndex = lineMetadata.findIndex(
+        (m) => m.metrics.startIndex <= head && head <= m.metrics.endIndex,
+      );
+      const metadata = lineMetadata[metadataIndex];
+      const targetMetadata = lineMetadata[metadataIndex + directionMultiplier];
+
+      // First or last line
+      if (!targetMetadata) {
+        switch (direction) {
+          case 'backward':
+            return 0;
+          case 'forward':
+            return length;
+        }
+      }
+
+      const shapedLine = metadata.shapedLine;
+
+      if (shapedLine) {
+        const coordinates = getGlyphCoordinatesForShapedLine(shapedLine);
+        const coordinate = coordinates[head - shapedLine.textRange.first];
+
+        return paragraph.getGlyphPositionAtCoordinate(
+          coordinate.x,
+          targetMetadata.metrics.baseline,
+        ).pos;
+      } else {
+        return targetMetadata.metrics.startIndex;
+      }
+    }
+  }
+}
+
 function getGlyphCoordinatesForShapedLine(shapedLine: ShapedLine) {
   const coordinates: Point[] = [];
 
@@ -275,4 +251,18 @@ function getGlyphCoordinatesForShapedLine(shapedLine: ShapedLine) {
   });
 
   return coordinates;
+}
+
+function getLineMetadata(paragraph: Paragraph) {
+  const shapedLines = paragraph.getShapedLines();
+  const lineMetrics = paragraph.getLineMetrics();
+
+  return lineMetrics.map((metrics) => {
+    return {
+      metrics,
+      shapedLine: shapedLines.find(
+        (shapeLine) => shapeLine.textRange.first === metrics.startIndex,
+      ),
+    };
+  });
 }
