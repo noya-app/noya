b37eea30a13861ee54644f1b3e6d3232c0af610a
Devin Abbott <devinabbott@gmail.com>
2021-08-14 18:09:17 -0700

    Add useMutableState hook

---
+++ packages/noya-react-utils/src/__tests__/useMutableState.test.ts
@@ -0,0 +1,34 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useMutableState } from '../hooks/useMutableState';
+
+test('uses mutable object', () => {
+  const object = { a: 1 };
+
+  const { result } = renderHook(() => useMutableState(object));
+
+  const proxy1 = result.current[0];
+  expect(proxy1).not.toBe(object);
+  expect(proxy1).toEqual(object);
+
+  act(() => {
+    result.current[1](() => {
+      object.a = 2;
+    });
+  });
+
+  const proxy2 = result.current[0];
+  expect(proxy2).not.toBe(object);
+  expect(proxy2).not.toBe(proxy1);
+  expect(proxy2).toEqual(object);
+  expect(proxy2).toEqual(proxy1);
+});
+
+test('uses mutable object with initializer', () => {
+  const object = { a: 1 };
+
+  const { result } = renderHook(() => useMutableState(() => object));
+
+  const proxy = result.current[0];
+  expect(proxy).not.toBe(object);
+  expect(proxy).toEqual(object);
+});
--- packages/noya-react-utils/src/index.ts
+++ packages/noya-react-utils/src/index.ts
@@ -2,3 +2,4 @@ export * from './hooks/useDeepArray';
 export * from './hooks/useIsMounted';
 export * from './hooks/useLazyValue';
 export * from './hooks/useShallowArray';
+export * from './hooks/useMutableState';
