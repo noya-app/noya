19575272e83a3dfc21814b2d0aa9535b26f143e2
Devin Abbott <devinabbott@gmail.com>
2021-08-18 16:57:04 -0700

    Fix theme text style rendering

--- packages/app/src/components/theme/TextStyle.tsx
+++ packages/app/src/components/theme/TextStyle.tsx
@@ -1,10 +1,10 @@
 import Sketch from '@sketch-hq/sketch-file-format-ts';
-import produce from 'immer';
 import { sketchColorToRgba } from 'noya-designsystem';
 import { center, Size } from 'noya-geometry';
 import { SketchLayer, useTextLayerParagraph } from 'noya-renderer';
 import { SketchModel } from 'noya-sketch-model';
-import { uuid } from 'noya-utils';
+import { Selectors } from 'noya-state';
+import produce from 'immer';
 import React, { memo, useMemo } from 'react';
 import CanvasGridItem from './CanvasGridItem';
 
@@ -23,42 +23,34 @@ function RCKTextStylePreview({
   size: Size;
 }) {
   const layer = useMemo(() => {
-    const layer = produce(SketchModel.text(), (draft) => {
-      draft.do_objectID = uuid();
-      draft.style = style;
-      draft.attributedString.string = name;
-
-      const encodedAttributes = draft.style.textStyle?.encodedAttributes;
-
-      if (encodedAttributes) {
-        draft.attributedString.attributes = [
+    const textStyle = produce(
+      style.textStyle ?? SketchModel.textStyle(),
+      (textStyle) => {
+        textStyle.encodedAttributes.paragraphStyle = SketchModel.paragraphStyle(
           {
-            _class: 'stringAttribute',
-            location: 0,
-            length: name.length,
-            attributes: encodedAttributes,
+            ...textStyle.encodedAttributes.paragraphStyle,
+            // Always center-align previews
+            alignment: Sketch.TextHorizontalAlignment.Centered,
           },
-        ];
+        );
+      },
+    );
 
-        encodedAttributes.paragraphStyle = encodedAttributes.paragraphStyle ?? {
-          _class: 'paragraphStyle',
-        };
-
-        // Always center-align previews
-        encodedAttributes.paragraphStyle.alignment =
-          Sketch.TextHorizontalAlignment.Centered;
-      }
+    return SketchModel.text({
+      style: { ...style, textStyle },
+      frame: SketchModel.rect({
+        x: 0,
+        y: 0,
+        width: size.width,
+        height: size.height,
+      }),
+      attributedString: Selectors.fromTextSpans([
+        {
+          string: name,
+          attributes: textStyle.encodedAttributes,
+        },
+      ]),
     });
-
-    layer.frame = {
-      ...layer.frame,
-      x: 0,
-      y: 0,
-      width: size.width,
-      height: size.height,
-    };
-
-    return layer;
   }, [size, style, name]);
 
   const paragraph = useTextLayerParagraph(layer);
