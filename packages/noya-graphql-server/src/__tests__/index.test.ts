6fc4b95bec4b7601d82c372440cb8733b888a587
Devin Abbott <devinabbott@gmail.com>
2021-08-19 11:27:38 -0700

    Use node to make http request to graphql API

--- packages/noya-graphql-server/src/__tests__/index.test.ts
+++ packages/noya-graphql-server/src/__tests__/index.test.ts
@@ -1,5 +1,5 @@
-import { execSync, exec } from 'child_process';
-import { sleep } from 'noya-utils';
+import { exec } from 'child_process';
+import http from 'http';
 import path from 'path';
 import webpack from 'webpack';
 
@@ -8,7 +8,7 @@ jest.setTimeout(60000);
 const root = path.join(__dirname, '../..');
 const webpackConfig = require('../../webpack.config.js');
 
-async function compile(env: unknown) {
+function compile(env: unknown) {
   return new Promise<void>((resolve, reject) => {
     webpack(webpackConfig(env), (error) => {
       if (error) {
@@ -20,20 +20,41 @@ async function compile(env: unknown) {
   });
 }
 
+function makeGraphQLQuery({ query }: { query: string }) {
+  return new Promise<unknown>((resolve, reject) => {
+    const options: http.RequestOptions = {
+      host: '127.0.0.1',
+      port: 4000,
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    };
+
+    const request = http.request(options, (response) => {
+      response.resume();
+      response.setEncoding('utf8');
+      let data = '';
+      response.on('error', reject);
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+      response.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+    });
+
+    request.write(JSON.stringify({ query }));
+    request.end();
+  });
+}
+
 test('server runs', async () => {
   await compile({ production: true });
 
   const process = exec('npm run start', { cwd: root });
 
-  await sleep(1000);
-
-  const response = execSync(
-    `curl --silent -X POST -H "Content-Type: application/json" --data '{ "query": "{ colors { id } }" }' http://localhost:4000/`,
-  );
+  const data = await makeGraphQLQuery({ query: '{ colors { id } }' });
 
   process.kill();
-
-  const data = JSON.parse(response.toString('utf8'));
 
   expect(data).toEqual({
     data: {
