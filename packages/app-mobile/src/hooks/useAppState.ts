76273aa67d6c56815d8503ecc92a2aabb3ca9e09
Michał Sęk <michal.sek@swmansion.com>
2022-01-20 20:28:20 +0100

    Configuration fixes once more

---
+++ packages/app-mobile/src/hooks/useAppState.ts
@@ -0,0 +1,14 @@
+// import { createInitialWorkspaceState, createSketchFile } from 'noya-state';
+// import { useCanvasKit } from 'noya-renderer';
+
+export default function useAppState() {
+  // const CanvasKit = useCanvasKit();
+
+  return {};
+}
+
+// useEffect(() => {
+//   const sketchFile = createSketchFile();
+
+//   const workspaceState = createInitialWorkspaceState(sketchFile);
+// }, []);
