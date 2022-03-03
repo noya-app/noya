8a9b52fe8b298366d5c08ccbe1da1c604621bf78
Michał Sęk <michal.sek@swmansion.com>
2022-03-03 16:13:44 +0100

    iocn

---
+++ packages/noya-designsystem/src/components/Layout/Icon/index.tsx
@@ -0,0 +1,5 @@
+import { memo } from 'react';
+
+export default memo(() => {
+  throw new Error('Icon.tsx not implemented!');
+});
---
+++ packages/noya-designsystem/src/components/Layout/index.ts
@@ -0,0 +1,5 @@
+import Icon from './Icon';
+
+export const Layout = {
+  Icon,
+};
