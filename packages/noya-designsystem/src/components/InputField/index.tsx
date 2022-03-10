27c7500a1e19df7af199c45125aae0fa09af277d
Michał Sęk <michal.sek@swmansion.com>
2022-03-10 14:33:14 +0100

    file for inputfield

--- packages/noya-designsystem/src/components/InputField/index.tsx
+++ packages/noya-designsystem/src/components/InputField/index.tsx
@@ -1,13 +1,1 @@
-import { memo } from 'react';
-
-import InputFieldNumberInput from './InputFieldNumberInput';
-import InputFieldLabel from './InputFieldLabel';
-import InputFieldRoot from './InputFieldRoot';
-import InputFieldInput from './InputFieldInput';
-
-export const InputField = {
-  Input: memo(InputFieldInput),
-  NumberInput: InputFieldNumberInput,
-  Label: InputFieldLabel,
-  Root: InputFieldRoot,
-};
+export { InputField } from './InputField';
