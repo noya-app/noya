259d6f775f461c991c4be32f8c95f44eb28e83c5
Devin Abbott <devinabbott@gmail.com>
2023-01-11 09:11:54 -0800

    Remove sandbox package

--- packages/sandbox/src/types.ts
+++
@@ -1,8 +0,0 @@
-import { Rect } from 'noya-geometry';
-import { DrawableLayerType } from 'noya-state';
-
-export type BlockHeuristicInput = { rect: Rect };
-export type InferredBlockTypeResult = {
-  type: DrawableLayerType;
-  score: number;
-};
