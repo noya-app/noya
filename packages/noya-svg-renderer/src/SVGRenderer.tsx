e432878db909ce3011495a627a5c9a11be728b4b
Devin Abbott <devinabbott@gmail.com>
2023-01-11 15:53:06 -0800

    Enable svg text element

--- packages/noya-svg-renderer/src/SVGRenderer.tsx
+++ packages/noya-svg-renderer/src/SVGRenderer.tsx
@@ -1,12 +1,17 @@
 import { Paint } from 'canvaskit';
-import { AffineTransform, Size } from 'noya-geometry';
+import { AffineTransform, Point, Size } from 'noya-geometry';
 import { ClipProps } from 'noya-react-canvaskit';
 import {
   ComponentsContextValue,
   ComponentsProvider,
   useCanvasKit,
 } from 'noya-renderer';
-import { Base64, detectFileType, getFileExtensionForType } from 'noya-utils';
+import {
+  Base64,
+  detectFileType,
+  getFileExtensionForType,
+  zip,
+} from 'noya-utils';
 import React, {
   ComponentProps,
   memo,
@@ -103,55 +108,53 @@ const Path: ComponentsContextValue['Path'] = memo(({ path, paint }) => {
 });
 
 const Text: ComponentsContextValue['Text'] = memo(({ rect, paragraph }) => {
-  // const rectProps = getRectProps(rect);
+  // To disable text for now:
+  // return null;
 
-  // const transform = useMemo(() => {
-  //   const affineTransform = AffineTransform.translation(
-  //     rectProps.x,
-  //     rectProps.y,
-  //   );
-  //   return stringifyAffineTransform(affineTransform);
-  // }, [rectProps.x, rectProps.y]);
+  const rectProps = getRectProps(rect);
 
-  return null;
+  const transform = useMemo(() => {
+    const affineTransform = AffineTransform.translate(rectProps.x, rectProps.y);
+    return stringifyAffineTransform(affineTransform);
+  }, [rectProps.x, rectProps.y]);
 
-  // const shapedLines = useMemo(() => paragraph.getShapedLines(), [paragraph]);
+  const shapedLines = useMemo(() => paragraph.getShapedLines(), [paragraph]);
 
-  // return (
-  //   <g transform={transform}>
-  //     {shapedLines.map((shapedLine, s) =>
-  //       shapedLine.runs.map((run, r) => {
-  //         const positions: Point[] = [];
+  return (
+    <g transform={transform}>
+      {shapedLines.map((shapedLine, s) =>
+        shapedLine.runs.map((run, r) => {
+          const positions: Point[] = [];
 
-  //         run.positions.forEach((value, index) => {
-  //           if (index % 2 === 0) {
-  //             positions.push({ x: value, y: 0 });
-  //           } else {
-  //             positions[positions.length - 1].y = value;
-  //           }
-  //         });
+          run.positions.forEach((value, index) => {
+            if (index % 2 === 0) {
+              positions.push({ x: value, y: 0 });
+            } else {
+              positions[positions.length - 1].y = value;
+            }
+          });
 
-  //         const strings = [...run.glyphs].map(
-  //           (value) => String.fromCharCode(value + 28), // TODO: Lookup by glyph id
-  //         );
+          const strings = [...run.glyphs].map(
+            (value) => String.fromCharCode(value + 28), // TODO: Lookup by glyph id
+          );
 
-  //         const info = zip(positions, strings);
+          const info = zip(positions, strings);
 
-  //         return info.map(([position, string], i) => (
-  //           <text
-  //             fontSize={12}
-  //             fill="white"
-  //             key={`${s}-${r}-${i}`}
-  //             x={position.x}
-  //             y={shapedLine.top + position.y}
-  //           >
-  //             {string}
-  //           </text>
-  //         ));
-  //       }),
-  //     )}
-  //   </g>
-  // );
+          return info.map(([position, string], i) => (
+            <text
+              fontSize={12}
+              fill="white"
+              key={`${s}-${r}-${i}`}
+              x={position.x}
+              y={shapedLine.top + position.y}
+            >
+              {string}
+            </text>
+          ));
+        }),
+      )}
+    </g>
+  );
 });
 
 const ClipPath = memo(
