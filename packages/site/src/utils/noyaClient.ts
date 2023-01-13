cd77b7527bf7f69bc10e8a798fcf8e5316341251
Devin Abbott <devinabbott@gmail.com>
2023-01-12 16:28:48 -0800

    Use production api host

--- packages/site/src/utils/noyaClient.ts
+++ packages/site/src/utils/noyaClient.ts
@@ -1,18 +1,21 @@
 import { NoyaAPI } from 'noya-api';
 
-// const networkClient = new NoyaAPI.NetworkClient({
-//   baseURI: 'http://localhost:31112/api',
-//   onError: (error) => {
-//     if (error instanceof NoyaAPI.Error && error.type === 'unauthorized') {
-//       window.location.href = 'http://localhost:31112/';
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   },
-// });
+const host = 'https://www.noya.io';
+// const host = 'http://localhost:31112';
 
-const networkClient = new NoyaAPI.LocalStorageClient();
+const networkClient = new NoyaAPI.NetworkClient({
+  baseURI: `${host}/api`,
+  onError: (error) => {
+    if (error instanceof NoyaAPI.Error && error.type === 'unauthorized') {
+      window.location.href = host;
+      return true;
+    } else {
+      return false;
+    }
+  },
+});
+
+// const networkClient = new NoyaAPI.LocalStorageClient();
 
 export const noyaClient = new NoyaAPI.Client({
   networkClient,
