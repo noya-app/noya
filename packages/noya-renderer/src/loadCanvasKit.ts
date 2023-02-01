314518d478eff1d6d6d506de474ab67e265dda87
Devin Abbott <devinabbott@gmail.com>
2023-02-01 10:58:00 -0800

    SVGRenderer should work with CanvasKit

--- packages/noya-renderer/src/loadCanvasKit.ts
+++ packages/noya-renderer/src/loadCanvasKit.ts
@@ -1,8 +1,15 @@
-import type { CanvasKitInit, Paint, PaintStyle } from 'canvaskit';
+import type {
+  CanvasKit,
+  CanvasKitInit,
+  Paint,
+  PaintStyle,
+  Shader,
+} from 'canvaskit';
 import { getPublicPath } from 'noya-public-path';
 
 declare module 'canvaskit' {
-  // Exposed for svgkit, where we need to be able to introspect the paint
+  // Exposed for SVGKit, since we need to introspect the paint in SVGRenderer.
+  // We need to implement these in CanvasKit too if we want to use with CanvasKit + SVGRenderer.
   interface Paint {
     style?: PaintStyle;
     _alpha?: number;
@@ -24,18 +31,32 @@ export function loadCanvasKit() {
       locateFile: (file: string) => getPublicPath() + 'wasm/' + file,
     });
 
-    const _setStyle = CanvasKit.Paint.prototype.setStyle;
-
-    CanvasKit.Paint.prototype.setStyle = function (
-      this: Paint,
-      paintStyle: PaintStyle,
-    ) {
-      this.style = paintStyle;
-      _setStyle.call(this, paintStyle);
-    };
+    patchForSVGRenderer(CanvasKit);
 
     resolve(CanvasKit);
   });
 
   return loadingPromise;
+}
+
+/**
+ * Expose properties on Paint for SVGRenderer. These properties don't have getters
+ * in CanvasKit, so we need to set them manually.
+ */
+function patchForSVGRenderer(CanvasKit: CanvasKit) {
+  const _setStyle = CanvasKit.Paint.prototype.setStyle;
+  const _setShader = CanvasKit.Paint.prototype.setShader;
+
+  CanvasKit.Paint.prototype.setStyle = function (
+    this: Paint,
+    paintStyle: PaintStyle,
+  ) {
+    this.style = paintStyle;
+    _setStyle.call(this, paintStyle);
+  };
+
+  CanvasKit.Paint.prototype.setShader = function (this: Paint, shader: Shader) {
+    this._shader = shader;
+    _setShader.call(this, shader);
+  };
 }
