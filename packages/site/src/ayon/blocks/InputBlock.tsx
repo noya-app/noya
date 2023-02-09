8f7eddbfeb1227f617faa86212c1aa80f2b9e300
David E. Chen <git@davec.net>
2023-02-09 01:05:16 -0800

    Input block hashtags for colors, disabled

--- packages/site/src/ayon/blocks/InputBlock.tsx
+++ packages/site/src/ayon/blocks/InputBlock.tsx
@@ -1,13 +1,38 @@
 import { Input } from '@chakra-ui/react';
 import { BlockDefinition } from 'noya-state';
 import React from 'react';
+import { parseBlock } from '../parse';
+import { getBlockThemeColors } from './colors';
 import { inputSymbolId } from './symbols';
+
+const globalHashtags = ['dark', 'accent', 'disabled'];
+
+const parser = 'regular';
 
 export const InputBlock: BlockDefinition = {
   id: inputSymbolId,
-  parser: 'regular',
+  parser,
+  hashtags: globalHashtags,
   infer: ({ frame, blockText }) => 0.1,
   render: (props) => {
-    return <Input value={props.blockText} />;
+    const {
+      content,
+      parameters: { dark, accent, disabled },
+    } = parseBlock(props.blockText, parser);
+
+    const { backgroundColor, color, borderColor } = getBlockThemeColors({
+      dark,
+      accent,
+    });
+
+    return (
+      <Input
+        value={content}
+        backgroundColor={backgroundColor}
+        color={color}
+        borderColor={borderColor}
+        isDisabled={!!disabled}
+      />
+    );
   },
 };
