5a2416746b159284f193983ffdc58c5356cf5ce8
David E. Chen <git@davec.net>
2023-02-08 22:47:30 -0800

    Checkbox block on/off, size, disabled

--- packages/site/src/ayon/blocks/CheckboxBlock.tsx
+++ packages/site/src/ayon/blocks/CheckboxBlock.tsx
@@ -1,16 +1,34 @@
 import { Checkbox } from '@chakra-ui/react';
 import { BlockDefinition } from 'noya-state';
 import React from 'react';
+import { parseBlock } from '../parse';
 import { isApproximatelySquare, isWithinRectRange } from './score';
 import { checkboxSymbolId } from './symbols';
 
+const placeholderText = '#off';
+
+const globalHashtags = ['on', 'off', 'disabled'];
+
+const parser = 'regular';
+
 export const CheckboxBlock: BlockDefinition = {
   id: checkboxSymbolId,
-  parser: 'regular',
+  parser,
+  hashtags: globalHashtags,
+  placeholderText,
   infer: ({ frame, blockText }) =>
     isWithinRectRange(frame, 10, 10, 20, 20) &&
     isApproximatelySquare(frame, 0.1)
       ? 0.8
       : 0,
-  render: (props) => <Checkbox />,
+  render: (props) => {
+    const {
+      parameters: { on, disabled },
+    } = parseBlock(props.blockText, parser, {
+      placeholder: placeholderText,
+    });
+    const size =
+      props.frame.height >= 20 ? 'lg' : props.frame.height >= 15 ? 'md' : 'sm';
+    return <Checkbox size={size} isChecked={!!on} isDisabled={!!disabled} />;
+  },
 };
