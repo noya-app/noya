a6a8d7ef08131332a2aab81f62b6945d87f0512c
David E. Chen <git@davec.net>
2023-02-08 22:16:02 -0800

    Switch block on/off, size, disabled

--- packages/site/src/ayon/blocks/SwitchBlock.tsx
+++ packages/site/src/ayon/blocks/SwitchBlock.tsx
@@ -1,11 +1,29 @@
 import { Switch } from '@chakra-ui/react';
 import { BlockDefinition } from 'noya-state';
 import React from 'react';
+import { parseBlock } from '../parse';
 import { switchSymbolId } from './symbols';
+
+const placeholderText = '#off';
+
+const globalHashtags = ['on', 'off', 'disabled'];
+
+const parser = 'regular';
 
 export const SwitchBlock: BlockDefinition = {
   id: switchSymbolId,
-  parser: 'regular',
+  parser,
+  hashtags: globalHashtags,
+  placeholderText,
   infer: ({ frame, blockText }) => 0.1,
-  render: (props) => <Switch />,
+  render: (props) => {
+    const {
+      parameters: { on, disabled },
+    } = parseBlock(props.blockText, parser, {
+      placeholder: placeholderText,
+    });
+    const size =
+      props.frame.height >= 35 ? 'lg' : props.frame.height >= 25 ? 'md' : 'sm';
+    return <Switch size={size} isChecked={!!on} isDisabled={!!disabled} />;
+  },
 };
