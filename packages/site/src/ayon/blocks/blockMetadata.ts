635419490003aac34b8e2fa5c83c62dbe6817d96
Devin Abbott <devinabbott@gmail.com>
2023-02-28 19:31:28 -0800

    More element pages

--- packages/site/src/ayon/blocks/blockMetadata.ts
+++ packages/site/src/ayon/blocks/blockMetadata.ts
@@ -1,6 +1,10 @@
 import { Size } from 'noya-geometry';
 import {
+  avatarSymbolId,
+  boxSymbolId,
+  buttonSymbolId,
   heroSymbolV2Id,
+  iconSymbolId,
   sidebarSymbolId,
   signInSymbolId,
   tableSymbolId,
@@ -17,10 +21,30 @@ export type BlockMetadata = {
 // This is in a separate file so docs don't have to import Chakra components directly.
 // Docs can import this synchronously and import(Ayon)
 export const blockMetadata: Record<string, BlockMetadata> = {
+  [avatarSymbolId]: {
+    name: 'Avatar',
+    category: 'element',
+    preferredSize: { width: 64, height: 64 },
+  },
+  [buttonSymbolId]: {
+    name: 'Button',
+    category: 'element',
+    preferredSize: { width: 200, height: 40 },
+  },
+  [boxSymbolId]: {
+    name: 'Box',
+    category: 'element',
+    preferredSize: { width: 200, height: 200 },
+  },
   [heroSymbolV2Id]: {
     name: 'Hero',
     category: 'marketing',
     preferredSize: { width: 600, height: 400 },
+  },
+  [iconSymbolId]: {
+    name: 'Icon',
+    category: 'element',
+    preferredSize: { width: 48, height: 48 },
   },
   [signInSymbolId]: {
     name: 'Sign In',
