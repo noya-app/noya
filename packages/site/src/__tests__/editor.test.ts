ff68fd5521994e0b26906f737e8c9668c249eaa9
Devin Abbott <devinabbott@gmail.com>
2023-06-24 17:11:40 -0700

    Add diagram in editor test

--- packages/site/src/__tests__/editor.test.ts
+++ packages/site/src/__tests__/editor.test.ts
@@ -4,15 +4,46 @@ import path from 'path';
 import {
   Descendant,
   Editor,
+  Selection,
   Element as SlateElement,
+  Node as SlateNode,
   createEditor,
 } from 'slate';
 import { withHistory } from 'slate-history';
 import { withReact } from 'slate-react';
+import { withOptions } from 'tree-visit';
 import { Blocks } from '../ayon/blocks/blocks';
 import { heroSymbolV2Id } from '../ayon/blocks/symbolIds';
 import { extractBlockContent, fromSymbol } from '../ayon/editor/serialization';
 import { withLayout } from '../ayon/editor/withLayout';
+
+const traverse = withOptions({
+  getChildren: (node: SlateNode): SlateNode[] => {
+    if (SlateElement.isElement(node) || Editor.isEditor(node)) {
+      return node.children;
+    }
+    return [];
+  },
+});
+
+function selectionToString(selection: Selection) {
+  if (!selection) return '(no selection)';
+  return `(${selection.anchor.path.join('.')}:${
+    selection.anchor.offset
+  } -> ${selection.focus.path.join('.')}:${selection.focus.offset})`;
+}
+
+const diagram = (node: SlateNode) => {
+  return traverse.diagram(node, (node) => {
+    if (Editor.isEditor(node)) {
+      return `Editor ${selectionToString(node.selection)}`;
+    } else if (SlateElement.isElement(node)) {
+      return node.label ?? '?';
+    } else {
+      return SlateNode.string(node) || '(empty)';
+    }
+  });
+};
 
 // Jest doesn't know how to import a text file, so we mock it
 jest.mock('../../safelist.txt', () => {
@@ -34,10 +65,7 @@ function toBlockName(node: Descendant) {
 test('serializes empty block', () => {
   const layer = SketchModel.symbolInstance({
     symbolID: heroSymbolV2Id,
-    frame: SketchModel.rect({
-      width: 400,
-      height: 400,
-    }),
+    frame: SketchModel.rect({ width: 400, height: 400 }),
   });
 
   const editor = withLayout(
@@ -71,4 +99,45 @@ test('serializes empty block', () => {
   ]);
 
   expect(editor.children).toEqual(nodes);
+});
+
+test('insert and delete text', () => {
+  const layer = SketchModel.symbolInstance({
+    symbolID: heroSymbolV2Id,
+    frame: SketchModel.rect({ width: 400, height: 400 }),
+  });
+
+  const editor = withLayout(
+    layer.symbolID,
+    withHistory(withReact(createEditor())),
+  );
+
+  Editor.normalize(editor, { force: true });
+
+  editor.selection = {
+    anchor: { path: [1, 0], offset: 0 },
+    focus: { path: [1, 0], offset: 0 },
+  };
+
+  expect(diagram(editor)).toMatchSnapshot('initial');
+
+  editor.insertText('hi');
+
+  expect(SlateNode.string(editor.children[1])).toEqual('hi');
+
+  expect(diagram(editor)).toMatchSnapshot('with hi');
+
+  editor.deleteBackward('character');
+
+  expect(SlateNode.string(editor.children[1])).toEqual('h');
+
+  editor.deleteBackward('character');
+
+  expect(SlateNode.string(editor.children[1])).toEqual('');
+
+  expect(diagram(editor)).toMatchSnapshot('delete');
+
+  editor.deleteBackward('character');
+
+  expect(diagram(editor)).toMatchSnapshot('final');
 });
