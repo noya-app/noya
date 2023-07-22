ea4bc4009243fd5454a0762e15df6e7043af4bf2
Devin Abbott <devinabbott@gmail.com>
2023-07-22 14:24:33 -0700

    Refactor DSEditor

--- packages/site/src/components/DSEditor.tsx
+++ packages/site/src/components/DSEditor.tsx
@@ -6,13 +6,10 @@ import {
   InputProps,
   LinkProps,
   RadioProps,
-  RenderableRoot,
   SelectProps,
   SwitchProps,
   TagProps,
-  Theme,
   component,
-  transform,
 } from '@noya-design-system/protocol';
 import { produce } from 'immer';
 import { DS } from 'noya-api';
@@ -33,18 +30,13 @@ import { InspectorPrimitives } from 'noya-inspector';
 import { loadDesignSystem } from 'noya-module-loader';
 import { SketchModel } from 'noya-sketch-model';
 import { upperFirst } from 'noya-utils';
-import React, {
-  CSSProperties,
-  useEffect,
-  useLayoutEffect,
-  useMemo,
-  useRef,
-} from 'react';
+import React, { CSSProperties, useEffect } from 'react';
 import styled from 'styled-components';
 import { heroSymbol } from '../ayon/symbols/composed/HeroSymbol';
 import { librarySymbolMap } from '../ayon/symbols/symbols';
 import { tailwindColors } from '../ayon/tailwind/tailwind.config';
 import { renderDynamicContent } from '../ayon/utils/renderDynamicContent';
+import { DSRenderer } from './DSRenderer';
 import { InspectorSection } from './InspectorSection';
 
 interface Props {
@@ -83,10 +75,7 @@ export function DSEditor({
   viewType,
 }: Props) {
   const theme = useDesignSystemTheme();
-
   const [state, setState] = React.useState(initialDocument);
-
-  const [root, setRoot] = React.useState<RenderableRoot | undefined>();
 
   const {
     source: { name: sourceName },
@@ -105,10 +94,7 @@ export function DSEditor({
 
   useEffect(() => {
     async function fetchLibrary() {
-      const system = await loadDesignSystem(sourceName, {
-        Function: ref.current!.contentWindow!['Function' as any] as any,
-        enableCache: false,
-      });
+      const system = await loadDesignSystem(sourceName);
 
       setSystem(system);
     }
@@ -117,152 +103,124 @@ export function DSEditor({
     fetchLibrary();
   }, [sourceName]);
 
-  const ref = useRef<HTMLIFrameElement>(null);
-
-  useEffect(() => {
-    if (system && !root) {
-      setRoot(system.createRoot(ref.current!.contentDocument!.body));
-    } else if (!system && root) {
-      root.unmount();
-      setRoot(undefined);
-    }
-  }, [root, system]);
-
-  const dsTheme = useMemo(() => {
-    if (!system || !system.themeTransformer) return undefined;
-
-    const t: Theme = {
-      colors: {
-        primary: (tailwindColors as any)[primary as any],
-        neutral: tailwindColors.slate,
-      },
-    };
-
-    const themeValue = transform({ theme: t }, system.themeTransformer);
-
-    return themeValue;
-  }, [primary, system]);
-
-  useLayoutEffect(() => {
-    if (!root || !system) return;
-
-    const Provider = system.components[component.id.Provider];
-    const Button: React.FC<ButtonProps> =
-      system.components[component.id.Button];
-    const Checkbox: React.FC<CheckboxProps> =
-      system.components[component.id.Checkbox];
-    const Radio: React.FC<RadioProps> = system.components[component.id.Radio];
-    const Select: React.FC<SelectProps> =
-      system.components[component.id.Select];
-    const Switch: React.FC<SwitchProps> =
-      system.components[component.id.Switch];
-    const Tag: React.FC<TagProps> = system.components[component.id.Tag];
-    const Link: React.FC<LinkProps> = system.components[component.id.Link];
-    const Input: React.FC<InputProps> = system.components[component.id.Input];
-    const Textarea: React.FC<InputProps> =
-      system.components[component.id.Textarea];
-    const Avatar: React.FC<AvatarProps> =
-      system.components[component.id.Avatar];
-
-    // const renderBox = boxSymbol.blockDefinition!.render!;
-
-    const getSymbolMaster = (symbolId: string) => librarySymbolMap[symbolId];
-
-    const hero = renderDynamicContent(
+  const handleRenderContent = React.useCallback(
+    ({
       system,
-      SketchModel.artboard({
-        layers: [
-          SketchModel.symbolInstance({
-            symbolID: heroSymbol.symbolID,
-          }),
-        ],
-      }),
-      getSymbolMaster,
-      undefined,
-      dsTheme,
-      'automatic-layout',
-    );
+      theme: dsTheme,
+    }: {
+      system: DesignSystemDefinition;
+      theme: any;
+    }) => {
+      const Button: React.FC<ButtonProps> =
+        system.components[component.id.Button];
+      const Checkbox: React.FC<CheckboxProps> =
+        system.components[component.id.Checkbox];
+      const Radio: React.FC<RadioProps> = system.components[component.id.Radio];
+      const Select: React.FC<SelectProps> =
+        system.components[component.id.Select];
+      const Switch: React.FC<SwitchProps> =
+        system.components[component.id.Switch];
+      const Tag: React.FC<TagProps> = system.components[component.id.Tag];
+      const Link: React.FC<LinkProps> = system.components[component.id.Link];
+      const Input: React.FC<InputProps> = system.components[component.id.Input];
+      const Textarea: React.FC<InputProps> =
+        system.components[component.id.Textarea];
+      const Avatar: React.FC<AvatarProps> =
+        system.components[component.id.Avatar];
 
-    const sectionStyle: CSSProperties = {
-      padding: '20px',
-      borderRadius: '4px',
-      background: 'white',
-      border: '1px solid #eee',
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '20px',
-      position: 'relative',
-    } as const;
+      const getSymbolMaster = (symbolId: string) => librarySymbolMap[symbolId];
 
-    const subSectionStyle = {
-      display: 'flex',
-      gap: '20px',
-      flexWrap: 'wrap',
-      alignItems: 'center',
-      position: 'relative',
-    } as const;
+      const hero = renderDynamicContent(
+        system,
+        SketchModel.artboard({
+          layers: [
+            SketchModel.symbolInstance({
+              symbolID: heroSymbol.symbolID,
+            }),
+          ],
+        }),
+        getSymbolMaster,
+        undefined,
+        dsTheme,
+        'automatic-layout',
+      );
 
-    const content = (
-      <div
-        style={{
-          padding: '20px',
-          gap: '12px',
-          display: 'flex',
-          flexDirection: 'column',
-          background: theme.colors.canvas.background,
-          minHeight: '100%',
-        }}
-      >
-        <span>Primitive Elements</span>
-        <div style={sectionStyle}>
-          <div style={subSectionStyle}>
-            <Button variant="solid">Button</Button>
-            <Button variant="outline">Button</Button>
-            <Button variant="text">Button</Button>
-            <Tag variant="solid">Tag</Tag>
-            <Tag variant="outline">Tag</Tag>
-            <Link href="#">Link</Link>
-            <Avatar
-              name="Devin Abbott"
-              style={{
-                width: '32px',
-                height: '32px',
-              }}
-            />
-            <Avatar
-              style={{
-                width: '32px',
-                height: '32px',
-              }}
-            />
+      const sectionStyle: CSSProperties = {
+        padding: '20px',
+        borderRadius: '4px',
+        background: 'white',
+        border: '1px solid #eee',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '20px',
+        position: 'relative',
+      } as const;
+
+      const subSectionStyle = {
+        display: 'flex',
+        gap: '20px',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        position: 'relative',
+      } as const;
+
+      const content = (
+        <div
+          style={{
+            padding: '20px',
+            gap: '12px',
+            display: 'flex',
+            flexDirection: 'column',
+            background: theme.colors.canvas.background,
+            minHeight: '100%',
+          }}
+        >
+          <span>Primitive Elements</span>
+          <div style={sectionStyle}>
+            <div style={subSectionStyle}>
+              <Button variant="solid">Button</Button>
+              <Button variant="outline">Button</Button>
+              <Button variant="text">Button</Button>
+              <Tag variant="solid">Tag</Tag>
+              <Tag variant="outline">Tag</Tag>
+              <Link href="#">Link</Link>
+              <Avatar
+                name="Devin Abbott"
+                style={{
+                  width: '32px',
+                  height: '32px',
+                }}
+              />
+              <Avatar
+                style={{
+                  width: '32px',
+                  height: '32px',
+                }}
+              />
+            </div>
+            <div style={subSectionStyle}>
+              <Checkbox checked label="Checkbox" />
+              <Radio checked label="Radio" />
+              <Select
+                options={['Option 1', 'Option 2', 'Option 3']}
+                value="Option 1"
+              />
+              <Switch checked />
+            </div>
+            <div style={subSectionStyle}>
+              <Input placeholder="Input" />
+              <Textarea placeholder="Textarea" />
+            </div>
           </div>
-          <div style={subSectionStyle}>
-            <Checkbox checked label="Checkbox" />
-            <Radio checked label="Radio" />
-            <Select
-              options={['Option 1', 'Option 2', 'Option 3']}
-              value="Option 1"
-            />
-            <Switch checked />
-          </div>
-          <div style={subSectionStyle}>
-            <Input placeholder="Input" />
-            <Textarea placeholder="Textarea" />
-          </div>
+          <span>Hero</span>
+          <div style={sectionStyle}>{hero}</div>
         </div>
-        <span>Hero</span>
-        <div style={sectionStyle}>{hero}</div>
-      </div>
-    );
+      );
 
-    const withProvider = Provider ? (
-      <Provider theme={dsTheme}>{content}</Provider>
-    ) : (
-      content
-    );
-
-    root.render(withProvider);
-  }, [dsTheme, root, state, system, theme.colors.canvas.background]);
+      return content;
+    },
+    [theme.colors.canvas.background],
+  );
 
   const inspector = (
     <Stack.V width="300px" background="white">
@@ -350,16 +308,6 @@ export function DSEditor({
           </InspectorSection>
           {system && (
             <InspectorSection title="Library Details" titleTextStyle="heading4">
-              {/* <InspectorPrimitives.LabeledRow label="Template">
-          <Text variant="code" fontSize="12px">
-            {sourceName}
-          </Text>
-        </InspectorPrimitives.LabeledRow>
-        <InspectorPrimitives.LabeledRow label="Template Version">
-          <Text variant="code" fontSize="12px">
-            {system.version}
-          </Text>
-        </InspectorPrimitives.LabeledRow> */}
               {system.dependencies && (
                 <Stack.V>
                   <InspectorPrimitives.SectionHeader>
@@ -399,32 +347,11 @@ export function DSEditor({
           <DividerVertical />
         </>
       )}
-      <Stack.V flex="1" position="relative">
-        <iframe
-          title="Design System Preview"
-          ref={ref}
-          srcDoc="<!DOCTYPE html>"
-          style={{
-            width: '100%',
-            height: '100%',
-          }}
-        />
-        {!system && (
-          <div
-            style={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              padding: '4px 8px',
-              color: '#aaa',
-              pointerEvents: 'none',
-            }}
-          >
-            Loading design system...
-          </div>
-        )}
-      </Stack.V>
+      <DSRenderer
+        sourceName={sourceName}
+        primary={primary}
+        renderContent={handleRenderContent}
+      />
     </Stack.H>
   );
 }
---
+++ packages/site/src/components/DSRenderer.tsx
@@ -0,0 +1,116 @@
+import {
+  DesignSystemDefinition,
+  ProviderProps,
+  RenderableRoot,
+  Theme,
+  component,
+  transform,
+} from '@noya-design-system/protocol';
+import { Stack } from 'noya-designsystem';
+import { loadDesignSystem } from 'noya-module-loader';
+import React, { useEffect, useLayoutEffect, useMemo, useRef } from 'react';
+import styled from 'styled-components';
+import { tailwindColors } from '../ayon/tailwind/tailwind.config';
+
+const Loading = styled.div({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  padding: '4px 8px',
+  color: '#aaa',
+  pointerEvents: 'none',
+});
+
+const Frame = styled.iframe({
+  width: '100%',
+  height: '100%',
+});
+
+export function DSRenderer({
+  sourceName,
+  primary,
+  renderContent,
+}: {
+  sourceName: string;
+  primary: string;
+  renderContent: (options: {
+    system: DesignSystemDefinition;
+    theme: any;
+  }) => React.ReactNode;
+}) {
+  const ref = useRef<HTMLIFrameElement>(null);
+  const [root, setRoot] = React.useState<RenderableRoot | undefined>();
+  const [system, setSystem] = React.useState<
+    DesignSystemDefinition | undefined
+  >();
+
+  useEffect(() => {
+    async function fetchLibrary() {
+      const system = await loadDesignSystem(sourceName, {
+        Function: ref.current!.contentWindow!['Function' as any] as any,
+        enableCache: false,
+      });
+
+      setSystem(system);
+    }
+
+    setSystem(undefined);
+    fetchLibrary();
+  }, [sourceName]);
+
+  useEffect(() => {
+    if (!system) {
+      if (root) {
+        root.unmount();
+        setRoot(undefined);
+      }
+
+      return;
+    }
+
+    if (!root) {
+      setRoot(system.createRoot(ref.current!.contentDocument!.body));
+    }
+  }, [root, system]);
+
+  const theme = useMemo(() => {
+    if (!system || !system.themeTransformer) return undefined;
+
+    const t: Theme = {
+      colors: {
+        primary: (tailwindColors as any)[primary as any],
+        neutral: tailwindColors.slate,
+      },
+    };
+
+    const themeValue = transform({ theme: t }, system.themeTransformer);
+
+    return themeValue;
+  }, [primary, system]);
+
+  useLayoutEffect(() => {
+    if (!root || !system) return;
+
+    const Provider: React.FC<ProviderProps> =
+      system.components[component.id.Provider];
+
+    const content = renderContent({ system, theme: theme });
+
+    const withProvider = Provider ? (
+      <Provider theme={theme}>{content}</Provider>
+    ) : (
+      content
+    );
+
+    root.render(withProvider);
+  }, [theme, renderContent, root, system]);
+
+  return (
+    <Stack.V flex="1" position="relative">
+      {/* Ensure html5 doctype for proper styling */}
+      <Frame ref={ref} title="Design System Preview" srcDoc="<!DOCTYPE html>" />
+      {!system && <Loading>Loading design system...</Loading>}
+    </Stack.V>
+  );
+}
