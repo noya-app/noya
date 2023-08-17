030ae3f3659a8e6d01cccc25484bdca0b407d4c0
Devin Abbott <devinabbott@gmail.com>
2023-08-17 10:58:43 -0700

    Add array diff

---
+++ packages/site/src/dseditor/__tests__/arrayDiff.test.ts
@@ -0,0 +1,139 @@
+import {
+  added,
+  applyArrayDiff,
+  computeArrayDiff,
+  moved,
+  removed,
+} from '../arrayDiff';
+
+test('append item', () => {
+  const a = ['a'];
+  const b = ['a', 'b'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([added('b', 1)]);
+});
+
+test('prepend item', () => {
+  const a = ['b'];
+  const b = ['a', 'b'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([added('a', 0)]);
+});
+
+test('inserts item', () => {
+  const a = ['b'];
+  const b = ['a', 'b', 'c'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([added('a', 0), added('c', 2)]);
+});
+
+test('removes item', () => {
+  const a = ['a', 'b'];
+  const b = ['a'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([removed(1)]);
+});
+
+test('replaces item', () => {
+  const a = ['a', 'b', 'd'];
+  const b = ['a', 'c', 'd'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([removed(1), added('c', 1)]);
+});
+
+test('replaces item with multiple changes', () => {
+  const a = ['a', 'b', 'd'];
+  const b = ['a', 'c', 'e'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([removed(1), removed(1), added('c', 1), added('e', 2)]);
+});
+
+test('swaps adjacent items', () => {
+  const a = ['a', 'b'];
+  const b = ['b', 'a'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([moved(0, 1)]);
+});
+
+test('swaps items', () => {
+  const a = ['a', 'x', 'b'];
+  const b = ['b', 'x', 'a'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([moved(0, 2), moved(0, 1)]);
+});
+
+test('moves item', () => {
+  const a = ['a', 'b', 'c'];
+  const b = ['b', 'c', 'a'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([moved(0, 2)]);
+});
+
+test('removes and moves item', () => {
+  const a = ['a', 'b', 'c'];
+  const b = ['c', 'a'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([removed(1), moved(0, 1)]);
+});
+
+test('moves and adds item', () => {
+  const a = ['a', 'b', 'c'];
+  const b = ['c', 'a', 'd'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([removed(1), added('d', 2), moved(0, 1)]);
+});
+
+test('swaps multiple pairs', () => {
+  const a = ['a', 'b', 'x', 'y', 'c', 'd'];
+  const b = ['b', 'a', 'x', 'y', 'd', 'c'];
+
+  const diff = computeArrayDiff(a, b);
+  const applied = applyArrayDiff(a, diff);
+
+  expect(applied).toEqual(b);
+  expect(diff).toEqual([moved(0, 1), moved(4, 5)]);
+});
---
+++ packages/site/src/dseditor/arrayDiff.ts
@@ -0,0 +1,103 @@
+export type ArrayDiffItem<T> =
+  | [type: 'a', item: T, to: number]
+  | [type: 'r', from: number]
+  | [type: 'm', from: number, to: number];
+
+export function added<T>(item: T, to: number): ArrayDiffItem<T> {
+  return ['a', item, to];
+}
+
+export function removed<T>(from: number): ArrayDiffItem<T> {
+  return ['r', from];
+}
+
+export function moved<T>(from: number, to: number): ArrayDiffItem<T> {
+  return ['m', from, to];
+}
+
+/**
+ * Assuming arrays of equal lengths, find the first move in the array.
+ */
+function computeFirstArrayMove<T>(
+  startIndex: number,
+  a: T[],
+  b: T[],
+  identity: (item: T) => unknown = (item) => item,
+): [number, ArrayDiffItem<T>] | undefined {
+  for (let i = startIndex; i < a.length; i++) {
+    if (identity(a[i]) !== identity(b[i])) {
+      return [
+        i,
+        moved(
+          i,
+          b.findIndex((item) => identity(item) === identity(a[i])),
+        ),
+      ];
+    }
+  }
+}
+
+export function computeArrayDiff<T>(a: T[], b: T[]): ArrayDiffItem<T>[];
+export function computeArrayDiff<T, K>(
+  a: T[],
+  b: T[],
+  identity: (item: T) => K,
+): ArrayDiffItem<T>[];
+export function computeArrayDiff<T>(
+  a: T[],
+  b: T[],
+  identity: (item: T) => unknown = (item) => item,
+): ArrayDiffItem<T>[] {
+  const diffs: ArrayDiffItem<T>[] = [];
+  const aMap = new Map(a.map((item, index) => [identity(item), index]));
+  const bMap = new Map(b.map((item, index) => [identity(item), index]));
+
+  // Check for removed items.
+  let removalOffset = 0;
+  for (let i = 0; i < a.length; i++) {
+    const item = a[i];
+    if (!bMap.has(identity(item))) {
+      diffs.push(removed(i - removalOffset));
+      removalOffset++;
+    }
+  }
+
+  // Check for added items.
+  for (let i = 0; i < b.length; i++) {
+    const item = b[i];
+    if (!aMap.has(identity(item))) {
+      diffs.push(added(item, i));
+    }
+  }
+
+  let result = applyArrayDiff(a, diffs);
+  let firstMove = computeFirstArrayMove(0, result, b, identity);
+  while (firstMove) {
+    diffs.push(firstMove[1]);
+    result = applyArrayDiff(result, [firstMove[1]]);
+    firstMove = computeFirstArrayMove(firstMove[0], result, b, identity);
+  }
+
+  return diffs;
+}
+
+export function applyArrayDiff<T>(a: T[], items: ArrayDiffItem<T>[]): T[] {
+  let result = [...a];
+
+  for (const item of items) {
+    switch (item[0]) {
+      case 'a':
+        result.splice(item[2], 0, item[1]);
+        break;
+      case 'r':
+        result.splice(item[1], 1);
+        break;
+      case 'm':
+        const [movedItem] = result.splice(item[1], 1);
+        result.splice(item[2], 0, movedItem);
+        break;
+    }
+  }
+
+  return result;
+}
